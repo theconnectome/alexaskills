2af4f24d51e77a412001779572886a10f71c515b
Ben Thomas <theconnectome@users.noreply.github.com>
2019-05-20 19:31:05 -0500

    Add files via upload

---
+++ search_wikipedia.py
@@ -0,0 +1,29 @@
+#!/bin/bash
+import wikipedia
+import urllib3
+import json
+
+def query_wikipedia():
+	# Take a text query from the user
+	query = input ("Query: ")
+
+	# Fetch the first Wikipedia result returned for that query
+	wiki_page = wikipedia.page(query)
+
+	# Extract that page's title, summary, and full text
+	wiki_title = wiki_page.title
+	wiki_summary = wikipedia.summary(query)
+	wiki_fullpage = wiki_page.content
+
+	# Print the intro and summary
+	return question("Here is the Wikipedia entry for " + wiki_title + ":\n" + wiki_summary + "\n Would you like me to continue reading?\n")
+
+@ask.intent("SearchWikipediaIntent")
+	query_wikipedia
+
+@ask.intent("YesIntent")
+	return statement(wiki_fullpage)
+	
+@ask.intent("NoIntent")
+	return statement("Okay.")
+	
