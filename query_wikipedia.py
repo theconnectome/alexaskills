53d96cbe32ba18eb729a1fcad856aee82ca35895
Ben Thomas <theconnectome@users.noreply.github.com>
2019-05-20 21:31:12 -0500

    Add files via upload

---
+++ query_wikipedia.py
@@ -0,0 +1,51 @@
+#!/bin/bash
+import wikipedia
+import urllib3
+import json
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import (
+    AbstractRequestHandler, AbstractExceptionHandler,
+    AbstractRequestInterceptor, AbstractResponseInterceptor)
+from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
+
+from ask_sdk_model.ui import SimpleCard
+from ask_sdk_model import Response
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+
+
+
+@ask.intent("SearchWikipediaIntent")
+def query_wikipedia():
+	# Take a text query from the user
+	query = input ("Query: ")
+
+	# Fetch the first Wikipedia result returned for that query
+	wiki_page = wikipedia.page(query)
+
+	# Extract that page's title, summary, and full text
+	wiki_title = wiki_page.title
+	wiki_summary = wikipedia.summary(query)
+	wiki_fullpage = wiki_page.content
+
+	# Print the intro and summary
+	return question("Here is the Wikipedia entry for " + wiki_title + ":\n" + wiki_summary + "\n Would you like me to continue reading?\n")
+
+@ask.intent("YesIntent")
+def full_page():
+	return statement(wiki_fullpage)
+	
+@ask.intent('AMAZON.StopIntent')
+def stop():
+	return statement("Goodbye")
+
+@ask.intent('AMAZON.CancelIntent')
+def cancel():
+	return statement("Goodbye")
+
+@ask.session_ended
+def session_ended():
+	return "{}", 200
