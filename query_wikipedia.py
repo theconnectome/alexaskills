b3466718f83f64c9941d66842c788fd959ab70c8
Ben Thomas <theconnectome@users.noreply.github.com>
2019-05-20 22:15:20 -0500

    Add files via upload

--- query_wikipedia.py
+++ query_wikipedia.py
@@ -29,7 +29,7 @@ fallback_message = "The Wikipedia skill can't help you with that. What can I hel
 fallback_reprompt = 'What can I help you with?'
 exception_message = "Sorry. I cannot help you with that."
 
-class QueryWikipediaHandler(AbstractRequestHandler)
+class QueryWikipediaHandler(AbstractRequestHandler):
 	# Handler for Query Wikipedia Intent.
 	def can_handle(self, handler_input):
 		return (is_request_type("LaunchRequest")(handler_input) or
@@ -56,124 +56,124 @@ class QueryWikipediaHandler(AbstractRequestHandler)
 
 
 class YesMoreInfoIntentHandler(AbstractRequestHandler):
-    # Handler for yes to more info intent.
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        session_attr = handler_input.attributes_manager.session_attributes
-        return (is_intent_name("AMAZON.YesIntent")(handler_input) and
+	# Handler for yes to more info intent.
+	def can_handle(self, handler_input):
+		# type: (HandlerInput) -> bool
+		session_attr = handler_input.attributes_manager.session_attributes
+		return (is_intent_name("AMAZON.YesIntent")(handler_input) and
                 "query" in session_attr)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        logger.info("In YesMoreInfoIntentHandler")
+	def handle(self, handler_input):
+		# type: (HandlerInput) -> Response
+		logger.info("In YesMoreInfoIntentHandler")
 
-        attribute_manager = handler_input.attributes_manager
-        session_attr = attribute_manager.session_attributes
-        _ = attribute_manager.request_attributes["_"]
+		attribute_manager = handler_input.attributes_manager
+		session_attr = attribute_manager.session_attributes
+		_ = attribute_manager.request_attributes["_"]
 
-        wiki_fullpage = wiki_page.content
-        speech = wiki_fullpage
-        handler_input.response_builder.speak(speech)
+		wiki_fullpage = wiki_page.content
+		speech = wiki_fullpage
+		handler_input.response_builder.speak(speech)
 		return handler_input.response_builder.response
 
 
 class HelpIntentHandler(AbstractRequestHandler):
-    # Handler for Help Intent.
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("AMAZON.HelpIntent")(handler_input)
+	# Handler for Help Intent.
+	def can_handle(self, handler_input):
+		# type: (HandlerInput) -> bool
+		return is_intent_name("AMAZON.HelpIntent")(handler_input)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        logger.info("In HelpIntentHandler")
+	def handle(self, handler_input):
+		# type: (HandlerInput) -> Response
+		logger.info("In HelpIntentHandler")
 
-        handler_input.response_builder.speak(help_message).ask(
-            help_reprompt).set_card(SimpleCard(
+		handler_input.response_builder.speak(help_message).ask(
+			help_reprompt).set_card(SimpleCard(
                 skill_name, help_message))
-        return handler_input.response_builder.response
+		return handler_input.response_builder.response
 
 
 class CancelOrStopIntentHandler(AbstractRequestHandler):
-    # Single handler for Cancel and Stop Intent.
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
+	# Single handler for Cancel and Stop Intent.
+	def can_handle(self, handler_input):
+		# type: (HandlerInput) -> bool
+		return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
                 is_intent_name("AMAZON.StopIntent")(handler_input))
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        logger.info("In CancelOrStopIntentHandler")
+	def handle(self, handler_input):
+		# type: (HandlerInput) -> Response
+		logger.info("In CancelOrStopIntentHandler")
 
-        handler_input.response_builder.speak(stop_message)
-        return handler_input.response_builder.response
+		handler_input.response_builder.speak(stop_message)
+		return handler_input.response_builder.response
 
 
 class FallbackIntentHandler(AbstractRequestHandler):
-    # Handler for Fallback Intent.
-    # AMAZON.FallbackIntent is only available in en-US locale.
-    # This handler will not be triggered except in that locale,
-    # so it is safe to deploy on any locale.
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("AMAZON.FallbackIntent")(handler_input)
+	# Handler for Fallback Intent.
+	# AMAZON.FallbackIntent is only available in en-US locale.
+	# This handler will not be triggered except in that locale,
+	# so it is safe to deploy on any locale.
+	def can_handle(self, handler_input):
+	# type: (HandlerInput) -> bool
+		return is_intent_name("AMAZON.FallbackIntent")(handler_input)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        logger.info("In FallbackIntentHandler")
+	def handle(self, handler_input):
+		# type: (HandlerInput) -> Response
+		logger.info("In FallbackIntentHandler")
 
-        handler_input.response_builder.speak(fallback_message).ask(
+		handler_input.response_builder.speak(fallback_message).ask(
             fallback_reprompt)
-        return handler_input.response_builder.response
+		return handler_input.response_builder.response
 
 
 class SessionEndedRequestHandler(AbstractRequestHandler):
-    # Handler for Session End.
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_request_type("SessionEndedRequest")(handler_input)
+	# Handler for Session End.
+	def can_handle(self, handler_input):
+		# type: (HandlerInput) -> bool
+		return is_request_type("SessionEndedRequest")(handler_input)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        logger.info("In SessionEndedRequestHandler")
+	def handle(self, handler_input):
+		# type: (HandlerInput) -> Response
+		logger.info("In SessionEndedRequestHandler")
 
-        logger.info("Session ended reason: {}".format(
+		logger.info("Session ended reason: {}".format(
             handler_input.request_envelope.request.reason))
-        return handler_input.response_builder.response
+		return handler_input.response_builder.response
 
 
 # Exception Handler
 class CatchAllExceptionHandler(AbstractExceptionHandler):
-    # Catch all exception handler, log exception and
-    # respond with custom message.
-    def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
-        return True
+	# Catch all exception handler, log exception and
+	# respond with custom message.
+	def can_handle(self, handler_input, exception):
+		# type: (HandlerInput, Exception) -> bool
+		return True
 
-    def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
-        logger.info("In CatchAllExceptionHandler")
-        logger.error(exception, exc_info=True)
+	def handle(self, handler_input, exception):
+		# type: (HandlerInput, Exception) -> Response
+		logger.info("In CatchAllExceptionHandler")
+		logger.error(exception, exc_info=True)
 
-        handler_input.response_builder.speak(exception_message).ask(
+		handler_input.response_builder.speak(exception_message).ask(
             exception_reprompt)
 
-        return handler_input.response_builder.response
+		return handler_input.response_builder.response
 
 
 # Request and Response loggers
 class RequestLogger(AbstractRequestInterceptor):
-    #Log the alexa requests.
-    def process(self, handler_input):
-        # type: (HandlerInput) -> None
-        logger.debug("Alexa Request: {}".format(
+	# Log the alexa requests.
+	def process(self, handler_input):
+		# type: (HandlerInput) -> None
+		logger.debug("Alexa Request: {}".format(
             handler_input.request_envelope.request))
 
 
 class ResponseLogger(AbstractResponseInterceptor):
-    #Log the alexa responses.
-    def process(self, handler_input, response):
-        # type: (HandlerInput, Response) -> None
-        logger.debug("Alexa Response: {}".format(response))
+	# Log the alexa responses.
+	def process(self, handler_input, response):
+		# type: (HandlerInput, Response) -> None
+		logger.debug("Alexa Response: {}".format(response))
 
 
 # Register intent handlers
