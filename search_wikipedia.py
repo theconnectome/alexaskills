18d0f8ab80aac83a6c0b50c15c21f9a2a607885e
Ben Thomas <theconnectome@users.noreply.github.com>
2019-05-20 20:15:38 -0500

    Add files via upload

--- search_wikipedia.py
+++ search_wikipedia.py
@@ -5,27 +5,42 @@ import json
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
 
-def query_wikipedia():
-	# Take a text query from the user
-	query = input ("Query: ")
+app = Flask(__name__)
+ask = Ask(app, '/')
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
-	# Fetch the first Wikipedia result returned for that query
-	wiki_page = wikipedia.page(query)
 
-	# Extract that page's title, summary, and full text
-	wiki_title = wiki_page.title
-	wiki_summary = wikipedia.summary(query)
-	wiki_fullpage = wiki_page.content
-
-	# Print the intro and summary
-	return question("Here is the Wikipedia entry for " + wiki_title + ":\n" + wiki_summary + "\n Would you like me to continue reading?\n")
 
 @ask.intent("SearchWikipediaIntent")
-	query_wikipedia
+	def query_wikipedia():
+		# Take a text query from the user
+		query = input ("Query: ")
+
+		# Fetch the first Wikipedia result returned for that query
+		wiki_page = wikipedia.page(query)
+
+		# Extract that page's title, summary, and full text
+		wiki_title = wiki_page.title
+		wiki_summary = wikipedia.summary(query)
+		wiki_fullpage = wiki_page.content
+
+		# Print the intro and summary
+		return question("Here is the Wikipedia entry for " + wiki_title + ":\n" + wiki_summary + "\n Would you like me to continue reading?\n")
 
 @ask.intent("YesIntent")
 	return statement(wiki_fullpage)
 	
-@ask.intent("NoIntent")
-	return statement("Okay.")
+@ask.intent('AMAZON.StopIntent')
+	def stop():
+		return statement("Goodbye")
+
+@ask.intent('AMAZON.CancelIntent')
+	def cancel():
+		return statement("Goodbye")
+
+@ask.session_ended
+	def session_ended():
+		return "{}", 200
 	
+if __name__ == '__main__':
+    app.run(debug=True)
